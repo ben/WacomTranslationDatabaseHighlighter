e5f178d827da1e8f51c4be0adb78011e5d7df1de
Ben Straub <ben@straubnet.net>
2010-02-27 21:05:19 -0800

    Version 1, works great.

---
+++ WacomTranslationDatabaseHighlighter.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+using System.Diagnostics;
+
+namespace WacomTranslationDatabaseHighlighter
+{
+    internal sealed class Components
+    {
+        [Export]
+        [FileExtension(".utf8")]
+        [ContentType("Wacom.TranslationDatabase")]
+        internal FileExtensionToContentTypeDefinition utf8FileExtensionDefinition;
+
+        [Export]
+        [Name("Wacom.TranslationDatabase")]
+        [BaseDefinition("text")]
+        internal static ContentTypeDefinition utf8ContentTypeDefinition;
+    }
+
+
+    #region Provider definition
+    /// <summary>
+    /// This class causes a classifier to be added to the set of classifiers. Since 
+    /// the content type is set to "text", this classifier applies to all text files
+    /// </summary>
+    [Export(typeof(IClassifierProvider))]
+    [ContentType("Wacom.TranslationDatabase")]
+    internal class WacomTranslationDatabaseHighlighterProvider : IClassifierProvider
+    {
+        /// <summary>
+        /// Import the classification registry to be used for getting a reference
+        /// to the custom classification type later.
+        /// </summary>
+        [Import]
+        internal IClassificationTypeRegistryService ClassificationRegistry = null; // Set via MEF
+
+        public IClassifier GetClassifier(ITextBuffer buffer)
+        {
+            return buffer.Properties.GetOrCreateSingletonProperty<WacomTranslationDatabaseHighlighter>(delegate { return new WacomTranslationDatabaseHighlighter(ClassificationRegistry); });
+        }
+    }
+    #endregion //provider def
+
+    #region Classifier
+    /// <summary>
+    /// Classifier that classifies all text as an instance of the OrinaryClassifierType
+    /// </summary>
+    class WacomTranslationDatabaseHighlighter : IClassifier
+    {
+        // Types for key and value
+        IClassificationType _keyType;
+        IClassificationType _valType;
+        IClassificationType _sepType;
+        IClassificationType _errType;
+
+        internal WacomTranslationDatabaseHighlighter(IClassificationTypeRegistryService registry)
+        {
+            _keyType = registry.GetClassificationType("WacomTranslationDatabaseHighlighter.Key");
+            _valType = registry.GetClassificationType("WacomTranslationDatabaseHighlighter.Value");
+            _sepType = registry.GetClassificationType("WacomTranslationDatabaseHighlighter.Separator");
+            _errType = registry.GetClassificationType("WacomTranslationDatabaseHighlighter.Error");
+        }
+
+        /// <summary>
+        /// This method scans the given SnapshotSpan for potential matches for this classification.
+        /// In this instance, it classifies everything and returns each span as a new ClassificationSpan.
+        /// </summary>
+        /// <param name="trackingSpan">The span currently being classified</param>
+        /// <returns>A list of ClassificationSpans that represent spans identified to be of this classification</returns>
+        public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
+        {
+            //create a list to hold the results
+            List<ClassificationSpan> classifications = new List<ClassificationSpan>();
+
+            // Split the line with the first tab character
+            string text = span.GetText();
+            int totalLength = text.Length;
+            int tabPosition = text.IndexOf('\t');
+
+            if (tabPosition < 0)
+            {
+                // No tab, error the entire line
+                return new List<ClassificationSpan>
+                {
+                    new ClassificationSpan(span, _errType)
+                };
+            }
+
+            if (tabPosition == 0)
+            {
+                // Tab at start of line, error just the tab
+                classifications.Add(new ClassificationSpan(
+                    new SnapshotSpan(span.Snapshot, new Span(span.Start, 1)),
+                    _errType));
+            }
+
+            if (tabPosition > 0)
+            {
+                classifications.Add(new ClassificationSpan(
+                    new SnapshotSpan(span.Snapshot, new Span(span.Start, tabPosition)),
+                    _keyType));
+                classifications.Add(new ClassificationSpan(
+                    new SnapshotSpan(span.Snapshot, new Span(span.Start + tabPosition, 1)),
+                    _sepType));
+                classifications.Add(new ClassificationSpan(
+                    new SnapshotSpan(span.Snapshot,
+                        new Span(span.Start + tabPosition + 1, totalLength - tabPosition - 1)),
+                    _valType));
+            }
+
+            int secondtabPosition = text.IndexOf('\t', tabPosition+1);
+            if (secondtabPosition > 0)
+            {
+                // Too many tabs, error starting with the second
+                classifications.Add(new ClassificationSpan(
+                    new SnapshotSpan(span.Snapshot,
+                        new Span(span.Start + secondtabPosition, totalLength - secondtabPosition)),
+                    _errType));
+            }
+
+            return classifications;
+        }
+
+#pragma warning disable 67
+        // This event gets raised if a non-text change would affect the classification in some way,
+        // for example typing /* would cause the classification to change in C# without directly
+        // affecting the span.
+        public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
+#pragma warning restore 67
+    }
+    #endregion //Classifier
+}
---
+++ WacomTranslationDatabaseHighlighterFormat.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace WacomTranslationDatabaseHighlighter
+{
+    #region Format definition
+    /// <summary>
+    /// Defines an editor format for the WacomTranslationDatabaseHighlighter type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "WacomTranslationDatabaseHighlighter.Key")]
+    [Name("WacomTranslationDatabaseHighlighter.KeyFormat")]
+    [UserVisible(true)] //this should be visible to the end user
+    [Order(Before = Priority.Default)] //set the priority to be after the default classifiers
+    internal sealed class WacomTranslationDatabaseHighlighterKeyFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "WacomTranslationDatabaseHighlighter" classification type
+        /// </summary>
+        public WacomTranslationDatabaseHighlighterKeyFormat()
+        {
+            this.DisplayName = "WacomTranslationDatabaseHighlighter"; //human readable version of the name
+            this.BackgroundColor = Colors.LightBlue;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+    #endregion //Format definition
+
+    #region Value Format definition
+    /// <summary>
+    /// Defines an editor format for the WacomTranslationDatabaseHighlighter type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "WacomTranslationDatabaseHighlighter.Value")]
+    [Name("WacomTranslationDatabaseHighlighter.ValueFormat")]
+    [UserVisible(true)] //this should be visible to the end user
+    [Order(Before = Priority.Default)] //set the priority to be after the default classifiers
+    internal sealed class WacomTranslationDatabaseHighlighterValueFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "WacomTranslationDatabaseHighlighter" classification type
+        /// </summary>
+        public WacomTranslationDatabaseHighlighterValueFormat()
+        {
+            this.DisplayName = "WacomTranslationDatabaseHighlighter"; //human readable version of the name
+            this.BackgroundColor = Colors.LightGreen;
+        }
+    }
+    #endregion //Format definition
+
+    #region Separator Format definition
+    /// <summary>
+    /// Defines an editor format for the WacomTranslationDatabaseHighlighter type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "WacomTranslationDatabaseHighlighter.Separator")]
+    [Name("WacomTranslationDatabaseHighlighter.SeparatorFormat")]
+    [UserVisible(true)] //this should be visible to the end user
+    [Order(Before = Priority.Default)] //set the priority to be after the default classifiers
+    internal sealed class WacomTranslationDatabaseHighlighterSeparatorFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "WacomTranslationDatabaseHighlighter" classification type
+        /// </summary>
+        public WacomTranslationDatabaseHighlighterSeparatorFormat()
+        {
+            this.DisplayName = "WacomTranslationDatabaseHighlighter"; //human readable version of the name
+            this.BackgroundColor = Colors.Yellow;
+            this.TextDecorations = System.Windows.TextDecorations.Strikethrough;
+        }
+    }
+    #endregion //Format definition
+
+    #region Error Format definition
+    /// <summary>
+    /// Defines an editor format for the WacomTranslationDatabaseHighlighter type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "WacomTranslationDatabaseHighlighter.Error")]
+    [Name("WacomTranslationDatabaseHighlighter.ErrorFormat")]
+    [UserVisible(true)] //this should be visible to the end user
+    [Order(After = Priority.Default)] //set the priority to be after the default classifiers
+    internal sealed class WacomTranslationDatabaseHighlighterErrorFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Defines the visual format for the "WacomTranslationDatabaseHighlighter" classification type
+        /// </summary>
+        public WacomTranslationDatabaseHighlighterErrorFormat()
+        {
+            this.DisplayName = "WacomTranslationDatabaseHighlighter"; //human readable version of the name
+            this.BackgroundColor = Colors.OrangeRed;
+        }
+    }
+    #endregion //Format definition
+}
