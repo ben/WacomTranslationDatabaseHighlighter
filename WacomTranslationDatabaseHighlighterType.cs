4c2129503641e1763c599c03b2568d2cfb8b256a
Ben Straub <ben.straub@wacom.com>
2010-03-01 14:37:46 -0800

    Converting to tabs.

--- WacomTranslationDatabaseHighlighterType.cs
+++ WacomTranslationDatabaseHighlighterType.cs
@@ -4,25 +4,25 @@ using Microsoft.VisualStudio.Utilities;
 
 namespace WacomTranslationDatabaseHighlighter
 {
-    internal static class WacomTranslationDatabaseHighlighterClassificationKeyDefinition
-    {
-        /// <summary>
-        /// Defines the "WacomTranslationDatabaseHighlighter" classification type.
-        /// </summary>
-        [Export(typeof(ClassificationTypeDefinition))]
-        [Name("WacomTranslationDatabaseHighlighter.Key")]
-        internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterKeyType = null;
+	internal static class WacomTranslationDatabaseHighlighterClassificationKeyDefinition
+	{
+		/// <summary>
+		/// Defines the "WacomTranslationDatabaseHighlighter" classification type.
+		/// </summary>
+		[Export(typeof(ClassificationTypeDefinition))]
+		[Name("WacomTranslationDatabaseHighlighter.Key")]
+		internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterKeyType = null;
 
-        [Export(typeof(ClassificationTypeDefinition))]
-        [Name("WacomTranslationDatabaseHighlighter.Value")]
-        internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterValueType = null;
+		[Export(typeof(ClassificationTypeDefinition))]
+		[Name("WacomTranslationDatabaseHighlighter.Value")]
+		internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterValueType = null;
 
-        [Export(typeof(ClassificationTypeDefinition))]
-        [Name("WacomTranslationDatabaseHighlighter.Separator")]
-        internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterSeparatorType = null;
+		[Export(typeof(ClassificationTypeDefinition))]
+		[Name("WacomTranslationDatabaseHighlighter.Separator")]
+		internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterSeparatorType = null;
 
-        [Export(typeof(ClassificationTypeDefinition))]
-        [Name("WacomTranslationDatabaseHighlighter.Error")]
-        internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterErrorType = null;
-    }
+		[Export(typeof(ClassificationTypeDefinition))]
+		[Name("WacomTranslationDatabaseHighlighter.Error")]
+		internal static ClassificationTypeDefinition WacomTranslationDatabaseHighlighterErrorType = null;
+	}
 }
